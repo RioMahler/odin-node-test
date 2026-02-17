c8b4f4b54d9ea8ddcab63c1516939146103fda10
RioMahler <229354130+RioMahler@users.noreply.github.com>
2026-02-17 13:56:32 +0100

    Refactor code to work with express

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
+const PORT = 8080;
 
-  let path = "./views/";
-
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact-me":
-      path += "contact-me.html";
-      break;
-    default:
-      path += "404.html";
-      break;
+app.listen(PORT, (error) => {
+  // This is important!
+  // Without this, any startup errors will silently fail
+  // instead of giving you a helpful error message.
+  if (error) {
+    throw error;
   }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      res.end(data);
-    }
-  });
+  console.log(`My first Express app - listening on port ${PORT}!`);
 });
 
-server.listen(8080, "localhost", () => {});
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile("./views/contact-me.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
